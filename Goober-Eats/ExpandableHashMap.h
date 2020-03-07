8bcb11326b72eb0915c44f4d0b7acbd41de40290
Dan Lee <danlee01@ucla.edu>
2020-03-07 12:46:39 -0800

    Initial Commit

---
+++ Goober-Eats/ExpandableHashMap.h
@@ -0,0 +1,71 @@
+//
+//  ExpandableHashMap.h
+//  Goober-Eats
+//
+//  Created by Dan Lee on 3/5/20.
+//  Copyright © 2020 Dan Lee. All rights reserved.
+//
+
+#ifndef ExpandableHashMap_h
+#define ExpandableHashMap_h
+
+// ExpandableHashMap.h
+
+// Skeleton for the ExpandableHashMap class template.  You must implement the first six
+// member functions.
+
+template<typename KeyType, typename ValueType>
+class ExpandableHashMap
+{
+public:
+    ExpandableHashMap(double maximumLoadFactor = 0.5);
+    ~ExpandableHashMap();
+    void reset();
+    int size() const;
+    void associate(const KeyType& key, const ValueType& value);
+    
+    // for a map that can't be modified, return a pointer to const ValueType
+    const ValueType* find(const KeyType& key) const;
+    
+    // for a modifiable map, return a pointer to modifiable ValueType
+    ValueType* find(const KeyType& key)
+    {
+        return const_cast<ValueType*>(const_cast<const ExpandableHashMap*>(this)->find(key));
+    }
+    
+    // C++11 syntax for preventing copying and assignment
+    ExpandableHashMap(const ExpandableHashMap&) = delete;
+    ExpandableHashMap& operator=(const ExpandableHashMap&) = delete;
+    
+private:
+};
+
+ExpandableHashMap::ExpandableHashMap(double maximumLoadFactor)
+{
+}
+
+ExpandableHashMap::~ExpandableHashMap()
+{
+}
+
+void ExpandableHashMap::reset()
+{
+}
+
+int ExpandableHashMap::size() const
+{
+    return -999;  // Delete this line and implement this function correctly
+}
+
+void ExpandableHashMap::associate(const KeyType& key, const ValueType& value)
+{
+}
+
+const ValueType* ExpandableHashMap::find(const KeyType& key) const
+{
+    return nullptr;  // Delete this line and implement this function correctly
+}
+
+
+
+#endif /* ExpandableHashMap_h */
